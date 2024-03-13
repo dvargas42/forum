603c1deb1194bda245e7e59b46db12db07518329
dvargas42 <dvargas.eng@gmail.com>
2024-03-13 14:13:15 -0300

    feat: :sparkles: change pageable from lista method attribute

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,13 +1,16 @@
 package br.com.alura.forum.controller;
 
 import java.net.URI;
-import java.util.List;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -38,15 +42,18 @@ public class TopicosController {
 	private CursoRepository cursoRepository;
 
 	@GetMapping
-	public List<TopicoDTO> lista(String nomeCurso) {
-		List<Topico> topicos;
-
+	public Page<TopicoDTO> lista(
+		@RequestParam(required = false) String nomeCurso,
+		@PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.DESC) Pageable paginacao) {
+		//@RequestParam int pagina, @RequestParam int qtd, @RequestParam String ordenacao
+		//Pageable paginacao = PageRequest.of(pagina, qtd, Direction.ASC, ordenacao); // ativado na class MAIN
+		Page<Topico> topicos;
+		
 		if (nomeCurso == null) {
-			topicos = topicoRepository.findAll();
+			topicos = topicoRepository.findAll(paginacao);
 		} else {
-			topicos = topicoRepository.findByCursoNome(nomeCurso);
+			topicos = topicoRepository.findByCursoNome(nomeCurso, paginacao);
 		}
-
 		return TopicoDTO.converter(topicos);
 	}
 
@@ -94,6 +101,6 @@ public class TopicosController {
 			topicoRepository.deleteById(id);
 			return ResponseEntity.ok().build();
 		}
-		return ResponseEntity.notFound().build(); 
+		return ResponseEntity.notFound().build();
 	}
 }
