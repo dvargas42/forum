94684cb8151daabe3f2afe2054030d4b53e59908
dvargas42 <dvargas.eng@gmail.com>
2024-03-13 16:36:33 -0300

    feat: :tada: initial commit

---
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
@@ -0,0 +1,43 @@
+package br.com.alura.forum.config.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private AutenticacaoService autenticacaoService;
+
+    @Override // Configuracoes de autenticacao
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    @Override // Configuracoes de autorizacao
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/topicos").permitAll()
+                .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+                .anyRequest().authenticated()
+                .and().formLogin();
+    }
+
+    @Override // Configuracoes de recursos estaticos
+    public void configure(WebSecurity web) throws Exception {
+        // TODO Auto-generated method stub
+        super.configure(web);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new BCryptPasswordEncoder().encode("123456"));
+    }
+}
