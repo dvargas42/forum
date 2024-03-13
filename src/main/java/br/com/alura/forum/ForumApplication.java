e6cb7e8e6fd3fd69c589e9f6547d07c4a33e3c1a
dvargas42 <dvargas.eng@gmail.com>
2024-03-13 14:45:52 -0300

    feat: :sparkles: include EnableCaching annotation in ForumApplication

--- src/main/java/br/com/alura/forum/ForumApplication.java
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -2,10 +2,12 @@ package br.com.alura.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport //Para ativar o pageable
+@EnableCaching // Habilitar cache, acrescentar em PROD outra ferramenta como REDIS
 public class ForumApplication {
 
 	public static void main(String[] args) {
