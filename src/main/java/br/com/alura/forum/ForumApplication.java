f78cc0f17a4e54e90a8a3966c99d55f82566205f
dvargas42 <dvargas.eng@gmail.com>
2024-03-16 19:27:18 -0300

    feat: :sparkles: include comment to build war file

--- src/main/java/br/com/alura/forum/ForumApplication.java
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -12,9 +12,14 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableCaching // Habilitar cache, acrescentar em PROD outra ferramenta como REDIS
 @EnableSwagger2
 public class ForumApplication {
+	//extends SpringBootServletInitializer { //extensao para criar war (aqui e no POM)
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForumApplication.class, args);
 	}
 
+	// @Override // metodo para ativar o war
+	// protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	// 	return builder.sources(ForumApplication.class);
+	// }
 }
