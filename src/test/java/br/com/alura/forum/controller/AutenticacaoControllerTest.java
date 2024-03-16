4770331dd06514ce5fa81bc832fa129a50b852f6
dvargas42 <dvargas.eng@gmail.com>
2024-03-16 16:52:14 -0300

    feat: :fire: remove  AutoConfigureDataJpa and TestEntityManager  useless imports

--- src/test/java/br/com/alura/forum/controller/AutenticacaoControllerTest.java
+++ src/test/java/br/com/alura/forum/controller/AutenticacaoControllerTest.java
@@ -5,8 +5,6 @@ import java.net.URI;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
